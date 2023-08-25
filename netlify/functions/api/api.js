08d13807d53f28fdc7afb5ccabac465b01b7a3f4
Lucas Mendonça <lucassms9@hotmail.com>
2023-08-25 14:32:12 -0300

    add func

---
+++ netlify/functions/api/api.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const modoRoutes = require("../../../routes/modo");
+const estiloRoutes = require("../../../routes/estilo");
+const sugestaoRoutes = require("../../../routes/sugestao");
+const lugarRoutes = require("../../../routes/lugar");
+const cidadeRoutes = require("../../../routes/cidade");
+
+const app = express();
+app.use(express.json());
+app.use("/", (req, res) => res.send("Hello world"));
+app.use("/api/", modoRoutes);
+app.use("/api/", estiloRoutes);
+app.use("/api/", sugestaoRoutes);
+app.use("/api/", lugarRoutes);
+app.use("/api/", cidadeRoutes);
+
+const handler = serverless(app);
+
+module.exports = { handler };
