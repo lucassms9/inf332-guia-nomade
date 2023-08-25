7bbb5eda90f50ee925f804e9405a141739a9c51a
Lucas Mendonça <lucassms9@hotmail.com>
2023-08-25 14:26:26 -0300

    export

---
+++ .netlify/functions/api.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const modoRoutes = require("../../routes/modo");
+const estiloRoutes = require("../../routes/estilo");
+const sugestaoRoutes = require("../../routes/sugestao");
+const lugarRoutes = require("../../routes/lugar");
+const cidadeRoutes = require("../../routes/cidade");
+
+const app = express();
+app.use(express.json());
+app.use("/", (req, res) => res.send("Hello world"));
+app.use("/api/", modoRoutes);
+app.use("/api/", estiloRoutes);
+app.use("/api/", sugestaoRoutes);
+app.use("/api/", lugarRoutes);
+app.use("/api/", cidadeRoutes);
+
+module.exports.handler = serverless(app);
